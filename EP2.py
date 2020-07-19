ededc1e0791cc394080aeafd76a48963e12036e1
Caio Silvestre <caio.silvestre@usp.br>
2020-07-19 02:26:50 -0300

    Add files via upload

--- EP2.py
+++ EP2.py
@@ -6,6 +6,7 @@ from tkinter.filedialog import askopenfilename
 from tkinter import *
 from collections import defaultdict
 
+
 root = Tk()
 filename = askopenfilename()
 root.destroy()
@@ -16,7 +17,7 @@ Lines = file1.readlines()
 count = 0
 arestas = []
 for line in Lines:
-    if count == 0:
+    if count==0:
         linha1 = line
     elif count == 1:
         linha2 = line
@@ -25,45 +26,34 @@ for line in Lines:
     count += 1
 
 
-class Graph(object):
+    class Graph(object):
 
-    def __init__(self, arestas):
-        self.adj = defaultdict(set)
-        self.add_arestas(arestas)
+        def __init__(self, arestas):
+            self.adj = defaultdict(set)
+            self.add_arestas(arestas)
 
-    def get_arestas(self):
-        return [(k, v) for k in self.adj.keys() for v in self.adj[k]]
+        def get_arestas(self):
+            return [(k, v) for k in self.adj.keys() for v in self.adj[k]]
 
-    def add_arestas(self, arestas):
-        for u, v in arestas:
-            self.adj[u].add(v)
-            self.adj[v].add(u)
+        def add_arestas(self, arestas):
+            for u, v in arestas:
+                self.adj[u].add(v)
+                self.adj[v].add(u)
 
-    def get_graus(self):
-        dict_graus = {}
-        for each in self.adj:
-            dict_graus[each] = len(self.adj[each])
-        return dict_graus
-
-    def __len__(self):
-        return len(self.adj)
-
-    def __str__(self):
-        return '{}({})'.format(self.__class__.__name__, dict(self.adj))
-
+        def get_graus(self):
+            dict_graus = {}
+            for each in self.adj:
+                dict_graus[each] = len(self.adj[each])
+            return dict_graus
 
 grafo_grande = Graph(arestas)
+A = grafo_grande.get_arestas()
+Qtd_arestas = int(len(A)/2)
+print('Quantidade de arestas: ', Qtd_arestas)
 
 graus = grafo_grande.get_graus()
-
-A = grafo_grande.get_arestas()
-
-Qtd_arestas = int(len(A)/2)
-
 hist = pd.DataFrame(list(graus.items()), columns=['vertice', 'grau'])
-
 final = hist.groupby('grau').count()
-
 final = final.reset_index()
 
 sns.set_style('darkgrid')
